f915ca51449124f793878265f7abd24c46c42fad
Christian Chavez <christian.chavez@fisagrp.com>
2020-01-16 16:13:46 -0500

    anadido los cambios

--- projEjemplo/src/oata/HelloWorld.java
+++ projEjemplo/src/oata/HelloWorld.java
@@ -1,7 +1,18 @@
 package oata;
-
+import org.apache.log4j.Logger;
+//import org.apache.log4j.BasicConfigurator;
+/**
+ * Clase de HelloWorld
+ */
 public class HelloWorld {
+    /**
+     * es el logger
+     */
+    static Logger logger = Logger.getLogger(HelloWorld.class);
+    /** clase main */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //BasicConfigurator.configure();
+        //System.out.println("Hello World");
+        logger.info("Hello World");
     }
 }
